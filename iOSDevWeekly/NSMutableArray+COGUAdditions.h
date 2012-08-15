b3eec245de0e7c99fd1f65e74d6570cebfde8e45
Colin Guenther <coling@gmx.de>
2012-08-15 11:40:06 +0200

    Implemented mutable array that will initialize its elements in a lazy manner.

--- iOSDevWeekly/NSMutableArray+COGUAdditions.h
+++ iOSDevWeekly/NSMutableArray+COGUAdditions.h
@@ -31,4 +31,58 @@
 */
 - (void)cogu_replaceObject:(id)oldObject withObject:(id)anObject;
 
+
+/*!
+ @brief Inserts a given object at the end of the receiver over and over again until it matches the passed times number.
+ @param anObject The object to add to the end of the receiver's content. This value must not be nil.
+ <b>Important</b> Raises an NSInvalidArgumentException if anObject is nil.
+
+ @param times Tells the receiver how often to add the passed object.
+*/
+- (void)cogu_addObject:(id)anObject times:(NSUInteger)times;
+
+
+/*!
+ @brief Returns an object in the receiver at an arbitrary index.
+ @discussion If the index is not within the bounds of the receiver, the receiver will add placeholder objects until the index is within the bounds. After that the receiver inserts the default object at the index and returns it.
+ @param index An index into the receiver. Is allowed to be out of bounds.
+ @param defaultObject An object that will be added at the index if the index is out of bounds or if the index points to a placeholder object.
+ <b>Important</b> Raises an NSInvalidArgumentException if defaultObject is nil and the index is out of bounds.
+
+ @return The object located at the index.
+*/
+- (id)cogu_objectAtLazyInitializedIndex:(NSUInteger)index defaultObject:(id)defaultObject;
+
+
+/*!
+ @brief Replaces the object at index with anObject.
+ @discussion If the passed index is not within the bounds of the receiver, the receiver will add placeholder objects until the index is within the bounds. After that the receiver inserts the passed object at the passed index.
+ @param index The index of the object to be replaced. This value is allowed to exceed the bounds.
+ @param anObject The object with which to replace the object at the passed index. This value must not be nil.
+ <b>Important</b> Raises an NSInvalidArgumentException if anObject is nil.
+*/
+- (void)cogu_replaceObjectAtLazyInitializedIndex:(NSUInteger)index withObject:(id)anObject;
+
+
+/*!
+ @brief Returns an object in the receiver at an arbitrary index path.
+ @discussion If the index path is not within the bounds of the receiver, the receiver will add placeholder objects until the index path is within the bounds. After that the receiver inserts the default object at the index path and returns it.
+ @param indexPath An index path into the receiver. Is allowed to be out of bounds.
+ @param defaultObject An object that will be added at the index path if the index path is out of bounds or the if the index path points to a placeholder object.
+ <b>Important</b> Raises an NSInvalidArgumentException if defaultObject is nil and the index path is out of bounds.
+
+ @return The object located at the index path.
+ */
+- (id)cogu_objectAtLazyInitializedIndexPath:(NSIndexPath*)indexPath defaultObject:(id)defaultObject;
+
+
+/*!
+ @brief Replaces the object at indexPath with anObject.
+ @discussion If the index path is not within the bounds of the receiver, the receiver will add placeholder objects until the index path is within the bounds. After that the receiver inserts the object at the index path.
+ @param indexPath The index path of the object to be replaced. This value is allowed to exceed the bounds.
+ @param anObject The object with which to replace the object at the index path. This value must not be nil.
+ <b>Important</b> Raises an NSInvalidArgumentException if anObject is nil.
+ */
+- (void)cogu_replaceObjectAtLazyInitializedIndexPath:(NSIndexPath*)indexPath withObject:(id)anObject;
+
 @end
--- iOSDevWeekly/NSMutableArray+COGUAdditions.m
+++ iOSDevWeekly/NSMutableArray+COGUAdditions.m
@@ -30,4 +30,66 @@
     [self replaceObjectAtIndex:indexOfObjectToBeReplaced withObject:anObject];
 }
 
+
+- (void)cogu_addObject:(id)anObject times:(NSUInteger)times;
+{
+    for (NSUInteger i = 0; i < times; i++)
+        [self addObject:anObject];
+}
+
+
+- (id)cogu_objectAtLazyInitializedIndex:(NSUInteger)index defaultObject:(id)defaultObject;
+{
+    NSUInteger objectCount = self.count;
+    if (index >= objectCount) {
+        [self cogu_replaceObjectAtLazyInitializedIndex:index withObject:defaultObject];
+        return defaultObject;
+    }
+
+    id object = [self objectAtIndex:index];
+    if (object != [NSNull null])
+        return object;
+
+    [self cogu_replaceObjectAtLazyInitializedIndex:index withObject:defaultObject];
+    return defaultObject;
+}
+
+
+- (void)cogu_replaceObjectAtLazyInitializedIndex:(NSUInteger)index withObject:(id)anObject;
+{
+    NSUInteger objectCount = self.count;
+    if (index < objectCount)
+        return [self replaceObjectAtIndex:index withObject:anObject];
+
+    NSUInteger placeholderObjectCount = index - objectCount;
+    id placeholderObject = [NSNull null];
+    [self cogu_addObject:placeholderObject times:placeholderObjectCount];
+
+    [self insertObject:anObject atIndex:index];
+}
+
+
+- (id)cogu_objectAtLazyInitializedIndexPath:(NSIndexPath*)indexPath defaultObject:(id)defaultObject;
+{
+    NSUInteger numberOfSubArrays = indexPath.length - 1;
+    NSMutableArray* subArray = self;
+    for (NSUInteger indexPosition = 0; indexPosition < numberOfSubArrays; indexPosition++)
+        subArray = [subArray cogu_objectAtLazyInitializedIndex:[indexPath indexAtPosition:indexPosition] defaultObject:[NSMutableArray array]];
+
+    id object = [subArray cogu_objectAtLazyInitializedIndex:[indexPath indexAtPosition:numberOfSubArrays] defaultObject:defaultObject];
+
+    return object;
+}
+
+
+- (void)cogu_replaceObjectAtLazyInitializedIndexPath:(NSIndexPath*)indexPath withObject:(id)anObject;
+{
+    NSUInteger numberOfSubArrays = indexPath.length - 1;
+    NSMutableArray* subArray = self;
+    for (NSUInteger indexPosition = 0; indexPosition < numberOfSubArrays; indexPosition++)
+        subArray = [subArray cogu_objectAtLazyInitializedIndex:[indexPath indexAtPosition:indexPosition] defaultObject:[NSMutableArray array]];
+
+    [subArray cogu_replaceObjectAtLazyInitializedIndex:[indexPath indexAtPosition:numberOfSubArrays] withObject:anObject];
+}
+
 @end
